5b7979dbba81136e72bcf0aa4865be22011d7075
TEC - Guendulain Pablo <pablo.guendulain@tecval.com.ar>
2022-10-27 16:32:21 -0300

    controller terminado, sin testear

--- byma/src/main/java/controllers/UsuariosController.java
+++ byma/src/main/java/controllers/UsuariosController.java
@@ -9,7 +9,7 @@ import services.IUsuarioService;
 
 import java.util.List;
 
-@RequestMapping("/usuarios")
+@RequestMapping("/usuarios/api")
 @RestController
 public class UsuariosController {
 
@@ -18,14 +18,31 @@ public class UsuariosController {
 
 
     @RequestMapping(value = "", method=RequestMethod.GET)
-    public ResponseEntity<Usuario> get(){
+    public ResponseEntity<Usuario> listarUsuarios(){
         return new ResponseEntity(userService.findAll(), HttpStatus.OK);
     }
 
+    @RequestMapping(value = "/{id}", method=RequestMethod.GET)
+    public ResponseEntity<Usuario> obtenerUsuario(@PathVariable("id") Long id){
+        try {
+            return new ResponseEntity(userService.getUserById(id), HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+        }
+    }
 
     @RequestMapping(value="", method = RequestMethod.POST)
-    public ResponseEntity create(@RequestBody Usuario usuario){
+    public ResponseEntity crearUsuario(@RequestBody Usuario usuario){
         return new ResponseEntity(userService.save(usuario), HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value="/{id}", method = RequestMethod.PUT)
+    public ResponseEntity modificarUsuario(@PathVariable("id") Long id, @RequestBody Usuario usuarioModificado){
+        return new ResponseEntity(userService.updateUser(id, usuarioModificado), HttpStatus.OK);
+    }
+    @RequestMapping(value="/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity eliminarUsuario(@PathVariable("id") Long id){
+        return new ResponseEntity(userService.deleteUserById(id), HttpStatus.OK);
     }
 
 
